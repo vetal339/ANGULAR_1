01b8a31797fb35a5d0428cf77ada12cf8290bbaa
Vetal339 <vitalijkadylinskij@gmail.com>
2021-07-27 00:19:40 +0300

    dz1

---
+++ app-first/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  header1= 'Header 1 title'
+  header2= 'Header 2 title'
+}
